1919090921a40f988f9f8f7ceb95475c8f817f63
PGeorge1709 <phucthanh0917@gmail.com>
2024-11-19 00:28:43 +0700

    delete reduntdant file

--- src/components/Department/Department.js
+++
@@ -1,7 +0,0 @@
-const Department = (props) => {
-    return (
-        <div>department page</div>
-    )
-}
-
-export default Department;
--- src/components/Project/Participation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Participation = () => {
-  return (
-    <div>Participation   chlling</div>
-  )
-}
-
-export default Participation
